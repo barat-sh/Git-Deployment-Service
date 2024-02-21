294d9fc7b36feae4cff9f24cc896476f001e00da
Barath Elangovan <barath_elangovan@outlook.com>
2024-02-22 00:09:14 +0530

    basic express server

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,16 @@
-import express from "express";
-import cors from "cors";
+import express, { Request, Response } from "express";
 const app = express();
 app.use(express.json());
-app.use(cors);
+
+app.get("/", (req, res) => {
+  res.send("hola");
+});
+
+app.post("/deploy", (req: Request, res: Response) => {
+  const URL = req.body.gitURL;
+  console.log(URL);
+  res.json({ URL });
+});
 
 app.listen(3001, () => {
   console.log("** Hitting diployment server! **");
